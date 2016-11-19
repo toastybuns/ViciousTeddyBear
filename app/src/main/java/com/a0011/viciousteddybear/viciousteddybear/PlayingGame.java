ba979bd2fd3b793a73510fc5a169d5742b0378a2
toastybuns <darylseah@d142-058-175-106.wireless.sfu.ca>
2016-11-19 14:59:50 -0800

    Update update

--- app/src/main/java/com/a0011/viciousteddybear/viciousteddybear/PlayingGame.java
+++ app/src/main/java/com/a0011/viciousteddybear/viciousteddybear/PlayingGame.java
@@ -2,6 +2,7 @@ package com.a0011.viciousteddybear.viciousteddybear;
 
 import android.annotation.SuppressLint;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,12 @@ import android.graphics.drawable.AnimationDrawable;
 import android.widget.ImageView;
 import android.content.Intent;
 import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.content.Context;
+import android.widget.Toast;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -24,18 +31,48 @@ public class PlayingGame extends AppCompatActivity {
     private View mControlsView;
     private boolean mVisible;
     int points = 0;
+
+
+    private SensorManager mSensorManager;
+    private float mAccel; // acceleration apart from gravity
+    private float mAccelCurrent; // current acceleration including gravity
+    private float mAccelLast; // last acceleration including gravity
+
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+        public void onSensorChanged(SensorEvent se) {
+            float x = se.values[0];
+            float y = se.values[1];
+            float z = se.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+            if (mAccel > 50) {
+                points += 1;
+                TextView scoresheet = (TextView) findViewById(R.id.Scoreboard);
+                scoresheet.setText(String.valueOf(points));
+            }
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
     /**
      * Touch listener to use for in-layout UI controls to delay hiding the
      * system UI. This is to prevent the jarring behavior of controls going away
      * while interacting with activity UI.
      */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        if (e.getAction() == MotionEvent.ACTION_DOWN) {
             points += 1;
-            TextView scoresheet = findViewById(R.id.Scoreboard);
-            return false;
+            TextView scoresheet = (TextView) findViewById(R.id.Scoreboard);
+            scoresheet.setText(String.valueOf(points));
         }
+        return false;
     };
 
     @Override
@@ -44,12 +81,17 @@ public class PlayingGame extends AppCompatActivity {
 
         setContentView(R.layout.activity_playing_game);
 
-
         ImageView sampleImage = (ImageView) findViewById(R.id.fullscreen_content);
         sampleImage.setBackgroundResource(R.drawable.sample1);
         sampleImage.setImageDrawable(getResources().getDrawable(R.layout.playing_game_animation_1));
         AnimationDrawable frameAnimation = (AnimationDrawable) sampleImage.getDrawable();
         frameAnimation.start();
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
@@ -59,7 +101,6 @@ public class PlayingGame extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
