e41f43285ab2647b4b23a0a47158b4181051fb0f
Jo <joeyjot@gmail.com>
2016-11-19 11:05:31 -0800

    draws red rectangle around face in test image

--- app/src/main/java/com/a0011/viciousteddybear/viciousteddybear/FullscreenActivity.java
+++ app/src/main/java/com/a0011/viciousteddybear/viciousteddybear/FullscreenActivity.java
@@ -7,6 +7,23 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+//import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+//import android.os.Bundle;
+import android.util.SparseArray;
+//import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -106,6 +123,51 @@ public class FullscreenActivity extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //button?
+        Button btn = (Button) findViewById(R.id.process);
+        btn.setOnClickListener(new View.OnClickListener() {
+            //        @Override
+            public void onClick(View v){
+                //perform action
+                ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inMutable=true;
+                Bitmap myBitmap = BitmapFactory.decodeResource(
+                        getApplicationContext().getResources(),
+                        R.drawable.test1,
+                        options);
+
+                Paint myRectPaint = new Paint();
+                myRectPaint.setStrokeWidth(5);
+                myRectPaint.setColor(Color.RED);
+                myRectPaint.setStyle(Paint.Style.STROKE);
+
+                Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+                Canvas tempCanvas = new Canvas(tempBitmap);
+                tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+
+                FaceDetector faceDetector = new
+                        FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
+                        .build();
+                if(!faceDetector.isOperational()){
+                    new AlertDialog.Builder(v.getContext()).setMessage("Could not set up the face detector!").show();
+                    return;
+                }
+
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                SparseArray<Face> faces = faceDetector.detect(frame);
+
+                for(int i=0; i<faces.size(); i++) {
+                    Face thisFace = faces.valueAt(i);
+                    float x1 = thisFace.getPosition().x;
+                    float y1 = thisFace.getPosition().y;
+                    float x2 = x1 + thisFace.getWidth();
+                    float y2 = y1 + thisFace.getHeight();
+                    tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+                }
+                myImageView.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
+            }
+        });
     }
 
     @Override
@@ -160,4 +222,8 @@ public class FullscreenActivity extends AppCompatActivity {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
+
+
+
+
 }
